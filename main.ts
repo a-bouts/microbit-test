81d7883f43c32fafd5f3a83fe150f0a7115b3a9e
abouts <arnaud.bouts@gmail.com>
2021-07-03 12:22:42 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,9 @@ let right = 0
 let left = 0
 let danse = 0
 let sing = 0
+let strip = neopixel.create(DigitalPin.P0, 2, NeoPixelMode.RGB)
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+strip.showRainbow(1, 360)
 basic.forever(function () {
     if (danse == 1) {
         left = randint(-100, 100)
